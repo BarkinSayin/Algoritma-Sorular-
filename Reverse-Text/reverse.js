5ca1a87aa3dab1a9e8f9c61b9c1feaef1d6b2a0e
BarkinSayin <barkinsayin@hotmail.com>
2024-04-30 18:22:18 +0300

    create-reverse-function

--- Reverse-Text/reverse.js
+++ Reverse-Text/reverse.js
@@ -1,4 +1,14 @@
 // Soru 2
 // "Hello, world!" yazısını "olleH, dlrow!" olarak tersine çeviren fonksiyonu yazınız.
 
-const text = "Hello, world";
+const text = "Hello, world!";
+
+const writeReverse = (string) => {
+  let reverse = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+  return reverse;
+};
+
+console.log(writeReverse(text));
