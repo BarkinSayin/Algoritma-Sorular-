a38bbab882c1a664677561f138d5b84a80abe53a
BarkinSayin <barkinsayin@hotmail.com>
2024-05-02 00:14:26 +0300

    calculator button listeners

--- HesapMakinesi/index.js
+++ HesapMakinesi/index.js
@@ -1,28 +1,72 @@
 const input = document.getElementById("input");
 const buttons = document.getElementsByClassName("buttons");
-console.log(buttons);
+
+let num1 = "";
+let operator = "";
+
 [...buttons].map((button) => {
   button.addEventListener("click", () => {
-    if (button.innerText === "0") {
+    if (
+      button.innerText === "0" ||
+      button.innerText === "1" ||
+      button.innerText === "2" ||
+      button.innerText === "3" ||
+      button.innerText === "4" ||
+      button.innerText === "5" ||
+      button.innerText === "6" ||
+      button.innerText === "7" ||
+      button.innerText === "8" ||
+      button.innerText === "9"
+    ) {
       input.value += parseInt(button.innerText);
-    } else if (button.innerText === "1") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "2") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "3") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "4") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "5") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "6") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "7") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "8") {
-      input.value += parseInt(button.innerText);
-    } else if (button.innerText === "9") {
-      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "+") {
+      if (input.value !== "") {
+        num1 = parseInt(input.value);
+        operator = "+";
+        input.value = "";
+      }
+    } else if (button.innerText === "-") {
+      if (input.value !== "") {
+        num1 = parseInt(input.value);
+        operator = "-";
+        input.value = "";
+      }
+    } else if (button.innerText === "x") {
+      if (input.value !== "") {
+        num1 = parseInt(input.value);
+        operator = "*";
+        input.value = "";
+      }
+    } else if (button.innerText === "/") {
+      if (input.value !== "") {
+        num1 = parseInt(input.value);
+        operator = "/";
+        input.value = "";
+      }
+    } else if (button.innerText === "=") {
+      if (input.value !== "") {
+        const num2 = parseInt(input.value);
+        let result;
+        switch (operator) {
+          case "+":
+            result = num1 + num2;
+            break;
+          case "-":
+            result = num1 - num2;
+            break;
+          case "*":
+            result = num1 * num2;
+            break;
+          case "/":
+            result = num1 / num2;
+            break;
+          default:
+            result = "Error";
+        }
+        input.value = result;
+        num1 = "";
+        operator = "";
+      }
     }
   });
 });
