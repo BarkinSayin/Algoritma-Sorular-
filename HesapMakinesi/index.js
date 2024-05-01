50f6e2d41e7e4df96aa60ac9381720924a66bbe1
BarkinSayin <barkinsayin@hotmail.com>
2024-05-01 21:09:21 +0300

    all-number-button-listeners

--- HesapMakinesi/index.js
+++ HesapMakinesi/index.js
@@ -3,7 +3,25 @@ const buttons = document.getElementsByClassName("buttons");
 console.log(buttons);
 [...buttons].map((button) => {
   button.addEventListener("click", () => {
-    if (button.innerText === "1") {
+    if (button.innerText === "0") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "1") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "2") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "3") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "4") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "5") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "6") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "7") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "8") {
+      input.value += parseInt(button.innerText);
+    } else if (button.innerText === "9") {
       input.value += parseInt(button.innerText);
     }
   });
